e0e55bb6945d24d833d53b3ae49cca39c3b6da26
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-06-26 09:19:25 +0530

    push code on git

---
+++ src/routes/ratingRoutes.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const  RatingController=require ("../controller/ratingController.js");
+
+const rc=new RatingController();
+const router=express.Router();
+
+router.post("/submit",rc.addRating);
+router.get("/all",rc.getAllUniqueUsers);
+router.get("/:userId",rc.getAllRatings);
+
+module.exports=router;
